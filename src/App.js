8c6d2af918fb0ecdf654e8f8b991a0ff963537cf
Szilard Magyar <szilard.magyar@gmail.com>
2017-04-27 23:16:33 +0200

    add counter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
+import TeslaBattery from './containers/TeslaBattery';
+
+const counterDefaultVal = {
+  speed: {
+    title: 'Speed',
+    unit: 'mph',
+    step: 5,
+    min: 45,
+    max: 70
+  },
+  temperature: {
+    title: 'Outside Temperature',
+    unit: '•',
+    step: 10,
+    min: -10,
+    max: 40
+  }
+};
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app">
+        <Header />
+        <TeslaBattery counterDefaultVal={counterDefaultVal} />
       </div>
     );
   }
