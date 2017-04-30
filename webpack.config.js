0615cb1d9e6f8900e8fd1deeacd5117d75cf5c2c
Szilard Magyar <szilard.magyar@gmail.com>
2017-04-30 13:45:22 +0200

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     loaders: [{
       test: /\.js$/,
       exclude: /node_modules/,
-      loader: 'babel-loader',
+      loader: 'babel',
       query: {
         presets: ['es2015', 'react']
       }
